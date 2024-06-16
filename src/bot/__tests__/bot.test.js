981590cb5903e060846b2fffabf81a9973b8e631
Elton Rexha <elton.rexha123@outlook.com>
2024-06-16 11:09:13 +0200

    Add tests for bot

---
+++ src/bot/__tests__/bot.test.js
@@ -0,0 +1,188 @@
+import botCreator from '../modules/bot';
+import gameboard from '../../modules/gameboard';
+import shipCreator from '../../modules/ship';
+
+describe('No edge cases', () => {
+  it('places ship randomly (horizontaly)', () => {
+    jest.spyOn(Math, 'random').mockImplementation(() => 0.4);
+
+    const playerGameboard = gameboard(10, 10, shipCreator);
+    const botGameBoard = gameboard(10, 10, shipCreator);
+
+    const difficulty = 1;
+    const bot = botCreator(difficulty, playerGameboard, botGameBoard);
+
+    //We expect it to be vertical. 0 is horizontal and 1 is vertical when it generates an random number
+    expect(bot.play(3)).toBe(true);
+
+    const expectedShip = shipCreator(3);
+
+    expect(JSON.stringify(botGameBoard.getBoard()[3][7].ship)).toEqual(JSON.stringify(expectedShip));
+    expect(JSON.stringify(botGameBoard.getBoard()[3][6].ship)).toEqual(JSON.stringify(expectedShip));
+    expect(JSON.stringify(botGameBoard.getBoard()[3][8].ship)).toEqual(JSON.stringify(expectedShip));
+  });
+
+   it('places ship randomly (verticaly)', () => {
+     jest.spyOn(Math, 'random').mockImplementation(() => 0.6);
+
+     const playerGameboard = gameboard(10, 10, shipCreator);
+     const botGameBoard = gameboard(10, 10, shipCreator);
+
+     const difficulty = 1;
+     const bot = botCreator(difficulty, playerGameboard, botGameBoard);
+
+     //We expect it to be vertical. 0 is horizontal and 1 is vertical when it generates an random number
+     expect(bot.play(3)).toBe(true);
+
+     const expectedShip = shipCreator(3);
+
+     expect(JSON.stringify(botGameBoard.getBoard()[8][2].ship)).toEqual(
+       JSON.stringify(expectedShip)
+     );
+     expect(JSON.stringify(botGameBoard.getBoard()[9][2].ship)).toEqual(
+       JSON.stringify(expectedShip)
+     );
+     expect(JSON.stringify(botGameBoard.getBoard()[7][2].ship)).toEqual(
+       JSON.stringify(expectedShip)
+     );
+   });
+
+  it('makes an random attack if theres no ships in the enemy', () => {
+    jest.spyOn(Math, 'random').mockImplementation(() => 0.4);
+
+    const playerGameboard = gameboard(10, 10, shipCreator);
+    const botGameBoard = gameboard(10, 10, shipCreator);
+
+    const difficulty = 0.4;
+    const bot = botCreator(difficulty, playerGameboard, botGameBoard);
+
+    expect(bot.attack()).toBe(false);
+    expect(playerGameboard.getBoard()[3][3].isHit).toBe(true);
+  });
+
+  it('makes adjecent attacks if an ship was hited (based on the heatmap), difficulty = 1', () => {
+    jest.spyOn(Math, 'random').mockImplementation(() => 0.4);
+
+    const playerGameboard = gameboard(10, 10, shipCreator);
+    const botGameBoard = gameboard(10, 10, shipCreator);
+
+    playerGameboard.placeShip(3, 3, 3, false);
+
+    const difficulty = 1;
+    const bot = botCreator(difficulty, playerGameboard, botGameBoard);
+
+    const possibleAdjecentAttacks = [
+      { row: 3, col: 4 },
+      { row: 3, col: 2 },
+      { row: 4, col: 3 },
+      { row: 2, col: 3 },
+    ];
+
+    expect(bot.attack()).toBe(true);
+    expect(playerGameboard.getBoard()[3][3].isHit).toBe(true);
+    bot.attack();
+
+    const out = possibleAdjecentAttacks.map((possibleMove) => {
+      return playerGameboard.getBoard()[possibleMove.row][possibleMove.col].isHit;
+    });
+
+    let pass = false;
+    out.forEach((move) => {
+      //If we made an move
+      if (move && !pass) {
+        pass = true;
+        //If we somehow made more that one hit
+      } else if (move && pass) {
+        pass = false;
+      }
+    });
+
+    expect(pass).toBe(true);
+  });
+
+  it('makes adjecent attacks if an ship was hited (based on the heatmap), difficulty = 0.3', () => {
+    jest
+      .spyOn(Math, 'random')
+      .mockImplementationOnce(() => 0.3)
+      .mockImplementation(() => 0.3);
+
+    const playerGameboard = gameboard(10, 10, shipCreator);
+    const botGameBoard = gameboard(10, 10, shipCreator);
+
+    playerGameboard.placeShip(3, 3, 3, false);
+
+    //The bot should flip this to 0.7 and use that as the control for the heatmap randomization
+    const difficulty = 0.3;
+
+    const bot = botCreator(difficulty, playerGameboard, botGameBoard);
+
+    expect(bot.attack()).toBe(false);
+    bot.attack();
+
+    expect(playerGameboard.getBoard()[0][0].isHit).toBe(true);
+  });
+
+  it('can check weather theres enough place for attack', () => {
+    const botGameboard = gameboard(10, 10, shipCreator);
+    const playerGameboard = gameboard(2, 2, shipCreator);
+
+    //Difficulty does not mater because the ships are placed randomly
+    const difficulty = 1;
+    const bot = botCreator(difficulty, playerGameboard, botGameboard);
+
+    playerGameboard.recieveAttack(0, 0);
+    playerGameboard.recieveAttack(0, 1);
+    playerGameboard.recieveAttack(1, 0);
+
+    expect(bot.canAttack()).toBe(true);
+  });
+});
+
+describe('Edge cases', () => {
+  it('returns false if theres no space for placing', () => {
+    const botGameboard = gameboard(3, 3, shipCreator);
+    const playerGameboard = gameboard(10, 10, shipCreator);
+
+    //Difficulty does not mater because the ships are placed randomly
+    const difficulty = 1;
+
+    botGameboard.placeShip(1, 0, 3, false);
+    botGameboard.placeShip(1, 1, 3, false);
+    botGameboard.placeShip(1, 2, 3, false);
+
+    const bot = botCreator(difficulty, playerGameboard, botGameboard, 10, 10);
+    expect(bot.play(3)).toBe(false);
+  });
+
+  it('returns false if theres no space for attack', () => {
+    const botGameboard = gameboard(10, 10, shipCreator);
+    const playerGameboard = gameboard(2, 2, shipCreator);
+
+    //Difficulty does not mater because the ships are placed randomly
+    const difficulty = 1;
+    const bot = botCreator(difficulty, playerGameboard, botGameboard);
+
+    playerGameboard.recieveAttack(0, 0);
+    playerGameboard.recieveAttack(0, 1);
+    playerGameboard.recieveAttack(1, 0);
+    playerGameboard.recieveAttack(1, 1);
+
+    expect(bot.attack()).toBe(false);
+  });
+
+  it('can check weather theres enough place for attack (should return false)', () => {
+    const botGameboard = gameboard(10, 10, shipCreator);
+    const playerGameboard = gameboard(2, 2, shipCreator);
+
+    //Difficulty does not mater because the ships are placed randomly
+    const difficulty = 1;
+    const bot = botCreator(difficulty, playerGameboard, botGameboard);
+
+    playerGameboard.recieveAttack(0, 0);
+    playerGameboard.recieveAttack(0, 1);
+    playerGameboard.recieveAttack(1, 0);
+    playerGameboard.recieveAttack(1, 1);
+
+    expect(bot.canAttack()).toBe(false);
+  });
+});
