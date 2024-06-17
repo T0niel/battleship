a0b888ddf691eea0e58abf68b9a8a71e6ffe16de
Elton Rexha <elton.rexha123@outlook.com>
2024-06-17 16:17:05 +0200

    Add compoenent to index.js, change index.html

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './imgs/icons/moon.png';
+import '../node_modules/css-reset/reset.css';
+import './styles/index.css';
+import headerComponent from './components/header';
+
+function init(){
+    headerComponent();
+}
+
+init();
--- webpack.common.js
+++ webpack.common.js
@@ -26,6 +26,15 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 
