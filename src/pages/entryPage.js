80316e627e44429ad88c2c2e82ce053f0871b897
Elton Rexha <elton.rexha123@outlook.com>
2024-06-20 18:34:22 +0200

    Add event listener for playerVsPlayer

--- src/pages/entryPage.js
+++ src/pages/entryPage.js
@@ -1,25 +1,34 @@
+import endGame from '../components/endGame';
 import header from '../components/header';
+import playerVsPlayer from './playerVsPlayer';
 
 const body = document.querySelector('body');
 
 export default () => {
-    header();
+  body.classList.remove('transition')
+  header();
 
-    const btnContainer = document.createElement('div');
+  const btnContainer = document.createElement('div');
 
-    btnContainer.classList.add('btn-container')
+  btnContainer.classList.add('btn-container');
 
-    const btnPlayerVsPlayer = document.createElement('button');
-    const btnPlayerVsBot = document.createElement('button');
+  const btnPlayerVsPlayer = document.createElement('button');
+  const btnPlayerVsBot = document.createElement('button');
 
-    btnPlayerVsPlayer.textContent = 'Player vs Player';
-    btnPlayerVsPlayer.classList.add('player-vs-player-btn');
+  btnPlayerVsPlayer.textContent = 'Player vs Player';
+  btnPlayerVsPlayer.classList.add('player-vs-player-btn');
 
-    btnPlayerVsBot.textContent = 'Player vs Bot';
-    btnPlayerVsBot.classList.add('player-vs-bot-btn');
+  btnPlayerVsBot.textContent = 'Player vs Bot';
+  btnPlayerVsBot.classList.add('player-vs-bot-btn');
 
-    btnContainer.appendChild(btnPlayerVsPlayer);
-    btnContainer.appendChild(btnPlayerVsBot);
+  btnContainer.appendChild(btnPlayerVsPlayer);
+  btnContainer.appendChild(btnPlayerVsBot);
 
-    body.appendChild(btnContainer);
-}
+  body.appendChild(btnContainer);
+
+  btnPlayerVsPlayer.addEventListener('click', () => {
+    body.innerHTML = '';
+    body.classList.add('transition')
+    playerVsPlayer(10, 10);
+  });
+};
