3066da20a43d944ee5aafa6f7514bcae5cf19f02
Elton Rexha <elton.rexha123@outlook.com>
2024-06-20 09:18:47 +0200

    Add board.js module
    added board.js module to create the board dom element

---
+++ src/components/board.js
@@ -0,0 +1,24 @@
+export default (row, col) => {
+    const boardWrapper = document.createElement('div');
+    boardWrapper.classList.add('board-wrapper');
+
+    const board = document.createElement('div');
+    board.classList.add('board');
+    
+    for(let i = 0; i < row; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.setAttribute('data-row', i);
+        for(let j = 0; j < col; j++){
+            const col = document.createElement('div');
+            col.classList.add('square');
+            col.setAttribute('data-col', j);
+            row.appendChild(col);
+        }
+        board.appendChild(row);
+    }
+
+    boardWrapper.appendChild(board);
+    
+    return boardWrapper;
+};
