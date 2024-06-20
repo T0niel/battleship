d890eff90e09b71d1ae8f0690534583d4f48d9ea
Elton Rexha <elton.rexha123@outlook.com>
2024-06-20 18:20:52 +0200

    Add endGame.js for the end game

---
+++ src/components/endGame.js
@@ -0,0 +1,40 @@
+const body = document.querySelector('body');
+import entryPage from '../pages/entryPage';
+
+export default (messsage, currentPage, ...currentPageArgs) => {
+  body.innerHTML = '';
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('end-game-wrapper');
+
+  const header = document.createElement('h1');
+  header.textContent = messsage;
+
+  const buttonWrapper = document.createElement('div');
+  buttonWrapper.classList.add('button-wrapper');
+
+  const playAgainBtn = document.createElement('button');
+  playAgainBtn.classList.add('play-again-btn');
+  playAgainBtn.textContent = 'Play again';
+
+  playAgainBtn.addEventListener('click', () => {
+    body.innerHTML = '';
+    currentPage();
+  });
+
+  const back = document.createElement('button');
+  back.classList.add('back-btn');
+  back.textContent = 'back';
+
+  back.addEventListener('click', () => {
+    body.innerHTML = '';
+    entryPage(...currentPageArgs);
+  })
+
+  buttonWrapper.appendChild(playAgainBtn);
+  buttonWrapper.appendChild(back);
+
+  wrapper.appendChild(header);
+  wrapper.appendChild(buttonWrapper);
+
+  body.appendChild(wrapper);
+};
